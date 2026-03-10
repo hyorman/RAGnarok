a0664fd84575891180a6f24f2c00023b865af0e0
hyorman <orman.yasin@outlook.com>
2026-03-10 23:46:20 +0100

    fix publish

--- scripts/package-dev.js
+++ scripts/package-dev.js
@@ -48,7 +48,7 @@ installProcess.on('close', (code) => {
   }
 
   // Run vsce package
-  const vsceProcess = spawn('npx', ['vsce', 'package'], {
+  const vsceProcess = spawn('npx', ['vsce', 'package', '--allow-proposed-apis', 'embeddings'], {
     stdio: 'inherit',
     shell: true
   });
